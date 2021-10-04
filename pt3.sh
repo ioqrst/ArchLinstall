6d19b5f2237fabd59a78b19a4a4cace13c48f181
HelloImD <dmiitryl@gmail.com>
2021-10-04 11:47:09 +0300

    2

---
+++ pt3.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "Enter user name: " uname
+useradd -m -g users -G wheel -s /bin/bash $uname
+passwd $uname
+
+echo "uncomment %wheel ALL=(ALL) ALL (first after root)"
+EDITOR=nano visudo
